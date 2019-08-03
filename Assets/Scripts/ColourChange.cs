c19fc2052ca46c1c46aba41f740f51370f33e0f7
Matthew Dyet <matthew@diomades.com>
2019-08-03 11:08:18 +0800

    Basic Lighting Changes
    Tab to change, Q to reset to normal

---
+++ Assets/Scripts/ColourChange.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum ColorSetting { none, blue, red }
+
+public class ColourChange : MonoBehaviour
+{
+    public Color worldRed;
+    public Color worldBlue;
+    private ColorSetting curColor = ColorSetting.red;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyUp("tab"))
+        {
+            if (curColor == ColorSetting.red)
+            {
+                RenderSettings.ambientLight = worldBlue;
+                curColor = ColorSetting.blue;
+            }
+            else if (curColor == ColorSetting.blue || curColor == ColorSetting.none)
+            {
+                RenderSettings.ambientLight = worldRed;
+                curColor = ColorSetting.red;
+            }
+        }
+        if (Input.GetKeyUp("q"))
+        {
+            if (curColor != ColorSetting.none)
+            {
+                RenderSettings.ambientLight = Color.white;
+                curColor = ColorSetting.none;
+            }
+        }
+    }
+}
